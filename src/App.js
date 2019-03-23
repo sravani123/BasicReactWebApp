58f93b5f62387063d54a362e76b3dfb5da4df9d4
scheripa <scheripa@tibco.com>
2019-03-23 17:59:18 +0530

    Modified port name

--- src/App.js
+++ src/App.js
@@ -19,16 +19,18 @@ console.log(req.data);
 });
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
  });
  
 
  app.post("/url", (req, res, next) => {
    console.log("============");
    console.log(req.body);
-
-   const {name, email, message} = req.body; 
+   const name = req.body.name;
+   const email =  req.body.email;
+   const message=  req.body.message;
+  //  const {name, email, message} = req.body; 
    console.log('message======'+message);
    var mailOptions = {
     from: 'scheripa@tibco.com',
@@ -59,51 +61,3 @@ app.listen(3000, () => {
   res.json(["Tony","Lisa","Michael","Ginger","Food"]);
  });
  
-
-
-
-// var transport = {
-//   host: 'smtp.gmail.com',
-//   auth: {
-//     user: creds.USER,
-//     pass: creds.PASS
-//   }
-// }
-
-// var transporter = nodemailer.createTransport(transport)
-
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Server is ready to take messages');
-//   }
-// });
-
-// app.post('/send', (req, res, next) => {
-//   var name = req.body.name
-//   var email = req.body.email
-//   var message = req.body.message
-//   var content = `name: ${name} \n email: ${email} \n message: ${content} `
-
-//   var mail = {
-//     from: name,
-//     to: 'chsravani0208@gmail.com',  //Change to email address that you want to receive messages on
-//     subject: 'New Message from Contact Form',
-//     text: content
-//   }
-
-//   transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         msg: 'fail'
-//       })
-//     } else {
-//       res.json({
-//         msg: 'success'
-//       })
-//     }
-//   })
-// })
-
-// module.exports = router;
