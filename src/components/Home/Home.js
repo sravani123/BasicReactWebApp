b31fc3acf924da3f990f9c7b48cdfddfbef2c230
scheripa <scheripa@tibco.com>
2019-03-30 15:20:25 +0530

    modified form submission code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,13 +12,10 @@ import {
 
 
 class Home extends React.Component {
-  // const [selectedDate, handleDateChange] = useState(new Date());
 
   state = {
     open: false,
-    selectedDate: new Date(),
-    selectedTime : new Date(),
-    mailDetails :{
+      mailDetails :{
       name:"",
       email:'',
       message:''
@@ -27,21 +24,27 @@ class Home extends React.Component {
       name: '',
       email: '',
       mobileNumber:'',
-      addressOne:'',
-      addressTwo:'',
+      address:'',
       landMark:'',
-      selectedService:''
+      sravani:'',
+      selectedService:'',
+      selectedDate: new Date(),
+      selectedTime : new Date(),
   }
 
   };
-  handleDateChange = date => {
-    console.log(date);
-    this.setState({ selectedDate:date});
+  handleInputFieldChange = (event, name) => {
+    event.persist();
+    const { target } = event;
+    this.setState(preState => ({
+      ...preState,
+      serviceRequest: {
+        ...preState.serviceRequest,
+        [name]: target.value
+      }
+    }));
   };
-  handleTimeChange = time => {
-    console.log(time);
-    this.setState({ selectedTime: time });
-  };
+
   
   handleShow = () => {
     this.setState({ open: true });
@@ -50,85 +53,19 @@ class Home extends React.Component {
   handleClose = () => {
     this.setState({ open: false });
   };
-  handleSelectChange =({target: {name,value}}) => { 
-    console.log(value); 
-    this.setState({ 
-    serviceRequest: { 
-    [name]: value 
-    } 
-    }); 
-    }
-  // handleServiceTypeChange = event =>{
-  //   console.log(event.target.name);
-  //   console.log(event.target.value);
-
-  //   this.setState({ [event.target.name]: event.target.value });
-  // }
-  handleChange = event => {
-    event.persist();
-    console.log(event.target.value);
-    switch(event.target.id) {
-      case 'mobile-number':
-      this.setState(state => {
-        state.serviceRequest.mobileNumber = event.target.value
-        return state
-      })
-      break;
-      case 'name':
-      this.setState(state => {
-        state.serviceRequest.name = event.target.value
-        return state
-      })
-      break;
-      // this.setState(state => (state.serviceRequest.mobileNumber = '384239847', state));
-      case 'emailId':
-      this.setState(state => {
-        state.serviceRequest.email = event.target.value
-        return state
-      })
-      break;
-      case 'address-one':
-      console.log()
-      this.setState((state )=> {
-        state.serviceRequest.addressOne = event.target.value
-        return state
-      })
-      break;
-      case 'address-two':
-      this.setState((state ) => {
-        state.serviceRequest.addressTwo =   `\n` +event.target.value
-        return state
-      })
-      break;
-      case 'landmark':
-      this.setState((state ) => {
-        console.log('===============event is ================='+event.target.value);
-        state.serviceRequest.landMark =  `\n` +event.target.value
-        return state
-      })
-      break;
-      case 'hello':
-      this.setState((state ) => {
-        console.log('===============event is ================='+event.target.value);
-        state.serviceRequest.selectedService =   event.target.value
-        return state
-      })
-      break;
-      default:
-        return 'foo';
-    }
-  };
+  
 
    handleSubmit = (e) =>{
-     console.log('handle submmit is called ===>>');
-    let {selectedDate,selectedTime,serviceRequest: {name, email, mobileNumber,addressOne , addressTwo, landMark,selectedService}} = this.state;
+   
+
+    let {serviceRequest: {name, email, mobileNumber,address,landMark,selectedService,selectedDate,selectedTime,sravani}} = this.state;
     selectedTime = selectedTime ? selectedTime.getHours() +':' + selectedTime.getMinutes() : '';
     selectedDate = selectedDate? `${selectedDate.getDate()} /  ${selectedDate.getMonth()}` :'';
-
+    
     // e.preventDefault();
     axios({
         method: "POST", 
-        url:"http://localhost:3000/url", 
+        url:"http://ec2-54-197-36-134.compute-1.amazonaws.com:4000/url", 
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
@@ -136,13 +73,13 @@ class Home extends React.Component {
         data: {
             name: name,   
             email: email,  
-            message: `You have got a service request from the name : ${name} \n on Date: ${selectedDate}  \n Time: ${selectedTime} \n  email: ${email} \n  Contact details: ${mobileNumber} \n Address: ${addressOne} , \n ${addressTwo}, \n ${landMark}`
+            message: `You have got a service request from the name : ${name} \n Selected Service : ${selectedService} \n on Date: ${selectedDate}  \n Time: ${selectedTime} \n  email: ${email} \n  Contact details: ${mobileNumber} \n Address: ${address} ,  \n`
         }
     }).then((response)=>{
       console.log("============"+name);
       console.log(response);
       
-      this.handleClose();
+       this.handleClose();
         if (response.data.msg === 'success'){
             alert("Message Sent."); 
             this.resetForm()
@@ -154,33 +91,18 @@ class Home extends React.Component {
 
 
   render() {
-    // {name, email, ...rest} = {a: 10, b: 20, c: 30, d: 40}
-    const {selectedDate,selectedTime,serviceRequest: {name, email, mobileNumber,addressOne , addressTwo, landMark,selectedService}} = this.state;
-    // const {} = this.state; 
-
-    // const { name, email, mobileNumber,addressOne , addressTwo, landMark  } = serviceRequest;
-    // const shedulueForm =   { name, email, mobileNumber,addressOne , addressTwo, landMark };
+    const {serviceRequest} = this.state;
+    
     return (
       <div>
       
       <HomeTemplate 
         handleShow = {this.handleShow}
         handleClose = {this.handleClose}
-        handleSubmit = {this.handleSubmit}
-        name ={name}
-        email={email}
-        addressOne={addressOne} 
-        addressTwo={addressTwo}
-         mobileNumber={mobileNumber}
-         landMark ={landMark}
-         selectedService ={selectedService}
+        serviceRequest = {serviceRequest}
         DateFnsUtils ={DateFnsUtils}
-        handleDateChange ={this.handleDateChange}
-        handleTimeChange={this.handleTimeChange}
-        handleSelectChange ={this.handleSelectChange}
-        handleChange = {this.handleChange}
-        selectedDate = {selectedDate}
-        selectedTime = {selectedTime}
+        handleSubmit ={this.handleSubmit}
+        handleInput ={this.handleInputFieldChange}
         open = {this.state.open}
        /> 
      </div>
